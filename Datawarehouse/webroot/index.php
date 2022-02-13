0fc4a0db9a440ea1716838e7d219a42b40f204b1
Emil Bratt Børsting <emilbratt@gmail.com>
2022-02-13 15:11:12 +0100

    index

---
+++ Datawarehouse/webroot/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+table {
+  border-collapse: collapse;
+}
+td {
+  text-align: right;
+}
+</style>
+<body>
+
+<?php
+?>
+
+</body>
+</html>
