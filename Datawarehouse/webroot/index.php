9f0661a22ec13027868ef93e7610a3884c6d1a9d
Emil Bratt Børsting <emilbratt@gmail.com>
2022-02-14 09:57:11 +0100

    typing

--- Datawarehouse/webroot/index.php
+++ Datawarehouse/webroot/index.php
@@ -5,8 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once './applications/controller.php';
-$request = new Controller;
+require_once './applications/Request.php';
+$request = new Request;
 
 
 // remove the ugly signature that mess up the print_r on $_SERVER
