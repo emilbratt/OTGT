f7e036623ba972e9728089b9dbc9ca02c796ee09
Emil Bratt Børsting <emilbratt@gmail.com>
2022-02-14 09:54:20 +0100

    index in webroot

--- Datawarehouse/webroot/index.php
+++ Datawarehouse/webroot/index.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html>
-<style>
-table, th, td {
-  border:1px solid black;
-}
-table {
-  border-collapse: collapse;
-}
-td {
-  text-align: right;
-}
-</style>
-<body>
-
 <?php
-?>
+// temporary enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-</body>
-</html>
+
+require_once './applications/controller.php';
+$request = new Controller;
+
+
+// remove the ugly signature that mess up the print_r on $_SERVER
+$_SERVER['SERVER_SIGNATURE'] = '';
+// print out globals
+echo '<pre>';
+print_r($_SERVER);
+print_r($_GET);
+echo '</pre>';
