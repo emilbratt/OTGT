a8c27f8569c1fc8a806b120caefe2caa5661397b
Emil Bratt Børsting <emilbratt@gmail.com>
2022-03-04 21:19:21 +0100

    cleaning

--- Datawarehouse/server/cip_info/applications/placement/TemplatePlacement.php
+++ Datawarehouse/server/cip_info/applications/placement/TemplatePlacement.php
@@ -8,19 +8,4 @@ class TemplatePlacement extends Template {
     parent::__construct();
   }
 
-  public function css_svg () {
-    $this->css .= <<<EOT
-      svg{width:30%; height:auto;}
-    EOT;
-  }
-
-  public function html_svg () {
-    $this->html .= <<<EOT
-    <svg viewbox="0 0 50 60">
-      <polygon points="0 0 50 0 50 5 0 50" fill="#C000FF"/>
-      <polygon points="0 50 50 5 50 60 0 60" fill="#803698"/>
-    </svg>
-    EOT;
-  }
-
 }
