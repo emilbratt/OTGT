2933981fb39b5d9cec4e15ef93cdc71e8b96d8d0
Emil Bratt Børsting <emilbratt@gmail.com>
2022-03-06 16:23:58 +0100

    add example app for documentation

---
+++ Datawarehouse/server/cip_info/applications/example/Application.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ *
+ * ABOUT:
+ *  this is just an example for showing how to structure the application
+ *  for it to work correctly and integrate well with the framework
+ *  and should not normally be part of the web-server
+ *
+ */
+
+class Example {
+  /**
+   *
+   * HERE GOES ALL THE GLOBAL LOGIC FOR THIS SPECIFIC APPLICATION
+   * NOTICE HOW THE CLASSES INHERIT FROM THIS MAIN APPLICATION CLASS
+   *
+   */
+  protected $page = 'Eksempel'; // REMEMBER TO ADD THIS PAGE NAME SO THE CORRECT TOP MENU BAR IS HIGHLIGHET
+  protected $environment;
+  protected $template;
+  protected $navigation;
+
+  function __construct () {
+    require_once '../applications/Environment.php';
+    require_once '../applications/example/TemplateExample.php';
+    require_once '../applications/example/NavigationExample.php';
+
+    $this->environment = new Environment();
+    $this->navigation = new NavigationExample($this->environment);
+    $this->template = new TemplateExample();
+
+    /**
+     *
+     *  ADD NAVIGATION VALUES TO THE TOP NAV BAR THAT WILL SHOW ON ALL PAGES
+     * IN THIS APPLICATION
+     *
+     */
+    $this->template->top_navbar($this->navigation->top_nav_links, $this->page);
+  }
+
+}
+
+/**
+ *
+ *
+ * THE APPLICATIONS ENTRYPOINTS STARTING WITH Home ARE FOUND BELOW
+ *
+ *
+ */
+
+class Home extends Example {
+  /**
+   *
+   * THIS IS THE HOME DIRECTORY FOR THIS APPLICATION AND WILL BE
+   * LOADED BY DEFAULT UNLESS A SUB STRING IS ADDED TO THE QUERYSTRING
+   *
+   * THE ADDRESS TO THIS CLASS IS: http://host:port/example
+   *
+   */
+  public function run () {
+    $this->template->sub_navbar($this->navigation->sub_nav_links);
+    $this->template->print();
+  }
+
+}
+
+
+class SubDirOne extends Example {
+  /**
+   *
+   * THIS IS THE SUBDIRONE DIRECTORY FOR THIS APPLICATION AND WILL BE
+   * LOADED BY DEFAULT UNLESS A SUB STRING IS ADDED TO THE QUERYSTRING
+   *
+   * THE ADDRESS TO THIS CLASS IS: http://host:port/example/subdirone
+   *
+   */
+  public function run () {
+    $this->template->some_text_for_subdirone();
+    $this->template->print();
+  }
+}
+
+
+class SubDirTwo extends Example {
+  /**
+   * THE ADDRESS TO THIS CLASS IS: http://host:port/example/subdirtwo
+   */
+  public function run () {
+    $this->template->message('this text is added using the method "message" in the parent class');
+    $this->template->print();
+  }
+}
+
+class SubDirThree extends Example {
+  /**
+   * THE ADDRESS TO THIS CLASS IS: http://host:port/example/subdirthree
+   */
+  public function run () {
+    $this->template->title_left_and_right('using the parent method for showing left', 'and right title');
+    $this->template->print();
+  }
+}
---
+++ Datawarehouse/server/cip_info/applications/example/NavigationExample.php
@@ -0,0 +1,47 @@
+<?php
+
+
+/**
+ *
+ * THIS IS THE NAVIGATION PART WHERE MENUS WILL BE CREATED
+ * IT INHERITS THE MAIN MENUS FROM ../applications/Navigation.php
+ *
+ */
+
+require_once '../applications/Navigation.php';
+
+class NavigationExample extends Navigation {
+  /**
+   *
+   * PASS THE ENVIRONMENT CONFIG VARIABLE SO THAT IT CAN DYNAMICALLY
+   * ADD OR REMOVE MENU ENTRIES BASED ON THE ENVIRONMENT
+   *
+   */
+  function __construct ($environment) {
+    parent::__construct($environment);
+    /**
+     *
+     * SET THE CORRECT NAME FOR THE PAGE (AS FOUND IN application.php)
+     *
+     */
+    $this->home_sub = $this->top_nav_links['Eksempel'];
+    /**
+     *
+     * OPTIONALLY ADD A SUB NAVIGATION MENU THAT APPEARS UNDER THE
+     * TOP NAVIGATION BAR
+     *
+     */
+      $this->sub_nav_links = [
+      'Sub directory 1' => $this->home_sub . '/subdirone',
+      'Sub directory 2' => $this->home_sub . '/subdirtwo',
+      'Sub directory 3' => $this->home_sub . '/subdirthree',
+    ];
+    /*
+     *
+     * THIS MEANS WE NEED TO ADD CORRESPONDING CLASSES IN Application.php
+     * WITH SAME NAME (INCLUDING UPPERCASE CLASS NAMING CONVENTION)
+     *
+     */
+  }
+
+}
