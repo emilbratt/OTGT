0efa9dcd1058b0a1d1ecbe56286d55acdc5b45d4
Emil Bratt Børsting <emilbratt@gmail.com>
2022-03-07 11:15:10 +0100

    remove placement from production

--- Datawarehouse/server/cip_info/applications/Navigation.php
+++ Datawarehouse/server/cip_info/applications/Navigation.php
@@ -17,17 +17,16 @@ class Navigation {
   function __construct ($environment) {
     $this->environment = $environment;
     $this->address = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
-    $this->top_nav_links = [
-      'Hjem' => $this->address,
-      'Vare' => $this->address . '/find',
-      'Rapporter' => $this->address . '/reports',
-      'Plassering' => $this->address . '/placement',
-      'Kart' => $this->address . '/map',
-      'Om' => $this->address . '/about',
-    ];
+    $this->top_nav_links = array();
+    $this->top_nav_links['Hjem'] = $this->address ;
+    $this->top_nav_links['Vare'] = $this->address . '/find';
+    $this->top_nav_links['Rapporter'] = $this->address . '/reports';
+    $this->top_nav_links['Kart'] = $this->address . '/map';
+    $this->top_nav_links['Om'] = $this->address . '/about';
 
     if ($this->environment->developement('show_nav_links')) {
       // extra menu entries shown if environment set accordingly
+      $this->top_nav_links['Plassering'] = $this->address . '/placement';
       $this->top_nav_links['Utvikling'] = $this->address . '/developing';
       $this->top_nav_links['Instrukser'] = $this->address . '/instructions';
     }
