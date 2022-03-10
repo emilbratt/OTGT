cb469f58f96d3af9e0c18d0c6cf72f0e6dd40686
Emil Bratt Børsting <emilbratt@gmail.com>
2022-03-10 11:35:36 +0100

    typing

--- Scripts/raspberry_pi_kiosk/README.md
+++ Scripts/raspberry_pi_kiosk/README.md
@@ -2,12 +2,16 @@
 
 
 * install raspberry pi os lite
+* install git
+```
+sudo apt update && sudo apt install git -y
+```
 * run sudo raspi-config to:
- ```
- set locale settings
- setup ssh for connecting
- set autologin
- setup wifi if needed
+```
+set locale settings
+setup ssh for connecting
+set autologin
+setup wifi if needed
  ```
 * copy over the autostart and setuo.sh file to the same directory (or an esaier way: git clone this repo and navigate to this directory)
 * run setup.sh as pi user
--- Scripts/raspberry_pi_kiosk/setup.sh
+++ Scripts/raspberry_pi_kiosk/setup.sh
@@ -5,6 +5,10 @@ if [[ ! -f ./autostart ]]; then
   echo 'cannot find autostart script (should be in the same directory as this script'
   exit 1
 fi
+if [[ ! -f ./keyboard ]]; then
+  echo 'cannot find keyboard config (should be in the same directory as this script'
+  exit 1
+fi
 
 # set hostname
 NEW_HOSTNAME=$(cat /etc/hostname)
@@ -21,11 +25,17 @@ sudo apt-get install --no-install-recommends chromium-browser -y
 sudo apt-get install xbase-clients -y
 
 # set startup command for profile
-echo 'if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then startx; fi' >> /home/pi/.bash_profile
+cat /home/pi/.bash_profile | grep -q 'startx'
+if [[ $? -eq 1 ]]; then
+  echo 'if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then startx; fi' >> /home/pi/.bash_profile
+fi
 
 # copy over the autostart config for cor starting chromium correctly on x
-
 sudo cp -f ./autostart /etc/xdg/openbox/
+# copy over the keyboard config for noewegian keyboard
+sudo cp -f ./keyboard /etc/default/keyboard
+
+
 
 # reboot
 sudo reboot
