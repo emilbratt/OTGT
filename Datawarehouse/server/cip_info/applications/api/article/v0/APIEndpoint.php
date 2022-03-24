a3b0b0dff94c8aeafaf7189433bb89a26144eeb8
Emil Bratt Børsting <emilbratt@gmail.com>
2022-03-24 16:19:00 +0100

    article api

---
+++ Datawarehouse/server/cip_info/applications/api/article/v0/APIEndpoint.php
@@ -0,0 +1,68 @@
+<?php
+
+class APIEndpoint {
+
+  protected $data;
+  protected $request;
+  protected $http_response_code;
+
+  function __construct ($request) {
+    require_once '../applications/DatabaseRetail.php';
+    require_once '../applications/api/article/v0/APIQueryRetail.php';
+    $this->http_response_code = 404;
+    $this->data = '';
+    $this->request = $request;
+  }
+
+  public function run () {
+    switch ($this->request[0]) {
+      case "article_movement":
+        $this->article_movement();
+        break;
+      case "article_movement":
+        echo 'article sales not implemented'; die;
+        break;
+      case "article_imports":
+        echo "article imports not implemented"; die;
+        break;
+      default:
+        echo "whoops 404";
+        $this->http_response_code = 404;
+    }
+  }
+
+  public function get_data () {
+    return $this->data;
+  }
+
+  public function get_return_code () {
+    return $this->http_response_code;
+  }
+
+  private function article_movement () {
+    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+      $this->data = '';
+      $this->http_response_code = 404;
+      return;
+    }
+    if ( !(isset($this->request[1])) ) {
+      $this->http_response_code = 500;
+      return;
+    }
+    if ( !(is_numeric($this->request[1])) ) {
+      $this->http_response_code = 500;
+      return;
+    }
+    $article_id = $this->request[1];
+    $query = new APIQueryRetail();
+    $query->article_movement($article_id);
+    $database_retail = new DatabaseRetail();
+    $database_retail->select_multi_row($query->get());
+    $query = null;
+    if ($database_retail->result) {
+      $this->data = $database_retail->result;
+      $this->http_response_code = 200;
+    }
+  }
+
+}
