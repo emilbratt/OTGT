32efdf655e29d1d418f66e32fd28116ebcc3f077
Emil Bratt Børsting <emilbratt@gmail.com>
2022-04-22 14:21:52 +0200

    typing

--- Datawarehouse/server/cip_info/applications/api/APILog.php
+++ Datawarehouse/server/cip_info/applications/api/APILog.php
@@ -7,7 +7,16 @@ class APILog {
   }
 
   public function run () {
-    // here will logic for logging come
+    /**
+     * here will logic for logging come
+     * what to log:
+     *  sender ip
+     *  request method
+     *  http response code
+     *  api endpoint
+     *  any value for client logit (for example if db insert was successfull)
+     *  timestamp
+     */
   }
 
 }
