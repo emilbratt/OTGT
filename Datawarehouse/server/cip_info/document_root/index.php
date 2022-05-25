979978d8fa97d6236e7c039b440d33e36eb7e867
Emil Bratt Børsting <emilbratt@gmail.com>
2022-05-25 11:46:02 +0200

    add: documentation about the PHP builtin: ini_set()

--- Datawarehouse/server/cip_info/document_root/index.php
+++ Datawarehouse/server/cip_info/document_root/index.php
@@ -8,25 +8,12 @@
  *  add batch update placement for items
  */
 
- /**
-  *
-  * all requests regardless of URL or http method, goes through this file
-  * which means that every dependent file that is included will have to
-  * be referenced as of the relative path to THIS file
-  *
-  */
 
 
- /**
-  * load the envirement for system wide configurations, auth. and settings
-  * that is used globally in the repository
-  */
+// load the envirement for system wide configurations, auth. and settings
+// that is used globally in the repository
 const ENVIRONMENT_INI = '../../../../environment.ini';
-require_once '../applications/Environment.php';
-
-
-
-// checking if it is created on a new installation
+// checking if environment.ini exists
 if ( !(is_file(ENVIRONMENT_INI)) ) {
   echo 'create environment.ini from a copy of the environment.ini.template<br>';
   echo 'and fill in a value for each variable<br>';
@@ -37,6 +24,25 @@ if ( !(is_file(ENVIRONMENT_INI)) ) {
   echo 'script terminated';
   exit(1);
 }
+require_once '../applications/Environment.php';
+
+ /**
+  *
+  * all requests regardless of URL or http method, goes through this file
+  * which means that every dependent file that is included will have to
+  * be referenced as of the relative path to THIS file
+  *
+  */
+
+ /**
+  * WARNING:
+  * due to php ini settings being handeled in AppRequest.php,
+  * ABSOLUTELY NO LOGIC should be added to this file
+  *
+  * this file should remain untouched as errors before loading
+  * AppRequest.php wont be reported if this script fails
+  */
+
 
 // the main entrypoint
 require_once '../applications/AppRequest.php';
