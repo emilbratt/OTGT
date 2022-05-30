e1f177ac8ba11c50e4ba8373850304989803cd94
Emil Bratt Børsting <emilbratt@gmail.com>
2022-05-30 20:31:05 +0200

    cleaning

--- Datawarehouse/server/cip_info/document_root/index.php
+++ Datawarehouse/server/cip_info/document_root/index.php
@@ -3,7 +3,6 @@
 /**
  *
  * TODO:
- *  add auto fetch new note in home page (js)
  *  add graph report for turnover and compare with earlier
  *  add graph report for sales count pr hour/day/week etc. and compare with earlier
  *  add a live feed showing the status (relaxed, busy, very busy etc.) based on how many customers (how many recent sales) to notify if we need more staff in shop
