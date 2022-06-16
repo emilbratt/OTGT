de9ca24c98377dd4800ef7e645562247cfc2e830
Emil Bratt Børsting <emilbratt@gmail.com>
2022-06-16 08:41:28 +0200

    cleaning

--- Datawarehouse/server/cip_info/applications/find/Application.php
+++ Datawarehouse/server/cip_info/applications/find/Application.php
@@ -197,17 +197,6 @@ class BySearch extends Find {
 
 class ByShelf extends Find {
 
-  /*
-   * Not implemented yet..
-   *
-   * displays items on that shelf, or if shelf is omitted, in that room
-   * example:
-   *  A = all items in A
-   *  A-B = all items in A-B
-   *  A-B-10 = all items on shelf A-B-10
-   *
-   */
-
     public function run () {
       $this->template->css_result_set();
       if( isset($_GET['input_field_shelf']) ) {
