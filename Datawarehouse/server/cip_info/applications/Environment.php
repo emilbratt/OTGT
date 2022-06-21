f342563a6677a48dfc7bdb0ecd6cf52c765cda1f
Emil Bratt Børsting <emilbratt@gmail.com>
2022-06-21 14:01:36 +0200

    typing

--- Datawarehouse/server/cip_info/applications/Environment.php
+++ Datawarehouse/server/cip_info/applications/Environment.php
@@ -32,8 +32,8 @@ class Environment {
     return $this->env_config['datawarehouse'][$key];
   }
 
-  public function nextcloud_otgt ($key) {
-    return $this->env_config['nextcloud_otgt'][$key];
+  public function cloudstorage ($key) {
+    return $this->env_config['cloudstorage'][$key];
   }
 
   public function developement ($key) {
