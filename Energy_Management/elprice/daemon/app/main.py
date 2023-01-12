e694e38610dcee0726837152bec93afb8056ec40
Emil Bratt <emilbratt@gmail.com>
2023-01-12 06:42:48 +0100

    ADD: boilerplate for handeling sleeptime

--- Energy_Management/elprice/daemon/app/main.py
+++ Energy_Management/elprice/daemon/app/main.py
@@ -1,1 +1,24 @@
-print('hello world!')
+from timehandle import sleep
+
+def run_every_hour(hour: int) -> bool:
+    print(hour)
+
+    print('running function for every hour')
+
+    if hour == 14:
+        print('running function for 14:00')
+
+    if hour == 0:
+        print('running function for midnight (00:00)')
+
+    return True
+
+
+def main():
+    print('running main')
+    hour = sleep.until_next_hour()
+    run_every_hour(hour)
+
+
+while True:
+    main()
---
+++ Energy_Management/elprice/daemon/app/timehandle/sleep.py
@@ -0,0 +1,17 @@
+from time import sleep
+from datetime import datetime
+
+def until_next_hour() -> int:
+    '''
+        this function will pause script until the next full hour reached
+        e.g. if time is 14:25, this function will sleep for 35 minutes
+        until time is 15:00
+    '''
+
+
+    while True:
+        minutes_until_new_hour = 60 - datetime.now().minute
+        sleep_seconds = minutes_until_new_hour * 60
+        sleep(sleep_seconds)
+        if (datetime.now().hour == hour) or (hour == -1):
+            return datetime.now().hour
