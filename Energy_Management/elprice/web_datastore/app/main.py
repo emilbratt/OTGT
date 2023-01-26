86dcf077a7b18ea8dfb717cf20512bd21c38d99e
emilbratt <emilbratt@gmail.com>
2023-01-26 19:32:26 +0100

    ADD: envar get example

--- Energy_Management/elprice/web_datastore/app/main.py
+++ Energy_Management/elprice/web_datastore/app/main.py
@@ -7,6 +7,8 @@ from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse, StreamingResponse, Response
 
+from envars     import envar_get
+
 POST_TEST_RESPONSE = 'test OK'
 
 class TestModel(BaseModel):
@@ -18,6 +20,19 @@ app = FastAPI()
 @app.get("/test/get")
 def test_get():
     return {'hello': 'world', 'test': 'OK'}
+
+# getting ENVARS
+@app.get('/envar/HOST_ALLOW_UPLOAD_ELSPOT')
+def envar_HOST_ALLOW_UPLOAD_ELSPOT():
+    return {'time today': envar_get('HOST_ALLOW_UPLOAD_ELSPOT')}
+
+@app.get('/envar/HOST_ALLOW_UPLOAD_PLOT')
+def envar_HOST_ALLOW_UPLOAD_PLOT():
+    return {'time today': envar_get('HOST_ALLOW_UPLOAD_PLOT')}
+
+@app.get('/envar/HOST_ALLOW_UPLOAD_SENSOR')
+def envar_HOST_ALLOW_UPLOAD_SENSOR():
+    return {'time today': envar_get('HOST_ALLOW_UPLOAD_SENSOR')}
 
 @app.post("/test/post")
 def test_post(item: TestModel):
