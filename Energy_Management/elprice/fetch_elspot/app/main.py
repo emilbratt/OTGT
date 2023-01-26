ed61bbaf5e9b455644b544e98f62c96dd524f6f0
emilbratt <emilbratt@gmail.com>
2023-01-26 16:44:24 +0100

    small changes

--- Energy_Management/elprice/fetch_elspot/app/main.py
+++ Energy_Management/elprice/fetch_elspot/app/main.py
@@ -7,7 +7,7 @@ from nordpooldayahead import nordpooldayaheadinit
 class Application:
 
     STEP_DESC = {
-        1: 'Sleeping until next clock hour',
+        1: 'Sleeping until fetch time',
         2: 'Fetching tomorrows prices from nordpool',
         3: 'Confirming correct date',
         4: 'Confirming correct currency',
@@ -21,39 +21,39 @@ class Application:
         print('Application starttime', isodate.today_minutes())
         self.nordpool = nordpooldayaheadinit(envar_get)
         self.http_transfer = httpdatastoreinit(envar_get)
-        self.step = 1
-        self.fresh_restarts = 0
+
         self.re_try = False
         self.re_try_counter = 0
+        self.fresh_restarts = 0
+
         self.fetch_hour = int(envar_get('NORDPOOL_FETCH_HOUR'))
-        # skip step 1 to include fetching first round if passed fetch_hour time
-        if timeofday.hour() >= self.fetch_hour:
+        self.fetch_minute = int(envar_get('NORDPOOL_FETCH_MINUTE'))
+        self.step = 1
+        if timeofday.is_passed_time(hour=self.fetch_hour, minute=self.fetch_minute):
+            # skip sleep step if we are passed fetch_hour time today
             self.step = 2
 
-    def run(self):
+    def run(self) -> bool:
         '''
             each step represents one task, the next task might depend on it
 
             if one task fails, we halt for a bit and depending on task,
-            we keep re-trying a previous task or error out
-
+            we keep re-trying a designated previous task; or error out
         '''
-        last_step = self.step
+        last_step = self.step # store the current step in another var
         print('---Step ' + str(self.step) + '---')
         print(self.STEP_DESC[self.step])
         match self.step:
             case 1:
-                print('time now', timeofday.now())
-                hour = sleep.until_next_hour()
-                if hour >= self.fetch_hour:
-                    self.step = 2
+                sleep.until_time_of_day(hour=self.fetch_hour, minute=self.fetch_minute)
+                self.step = 2
             case 2:
                 if self.nordpool.fetch():
                     self.step = 3
                 else:
                     self.re_try = True; self.step = 2
             case 3:
-                if self.nordpool.confirm_date(isodate.today_plus_days(-1)):
+                if self.nordpool.confirm_date(isodate.today_plus_days(-3)):
                     self.step = 4
                 else:
                     self.re_try = True; self.step = 2
@@ -86,13 +86,19 @@ class Application:
                 print('some weird behavior occured, value for self.step =', self.step)
                 exit(1)
 
+        if not self.re_try:
+            print('OK')
+            return True
+
         # after x fresh restarts, exit -> this service needs attention/update
         if self.fresh_restarts > 5:
-            exit(last_step)
+            print('to many fresh restarts, current step', last_step)
+            print('Exit application')
+            exit(1)
 
         # if a step has taken multiple re-tries, error out and start fresh
         if self.re_try_counter > 5:
-            print('start fresh because error on step', last_step)
+            print('ERROR: starting fresh because step', last_step,'failed')
             self.step = 1
             self.re_try_counter = 0
             self.fresh_restarts += 1
@@ -100,17 +106,18 @@ class Application:
 
         # if a step needs a re-try, halt until next 15 minute mark
         if self.re_try:
-            print('re-run from step', self.step, 'because of failed step', last_step)
+            print('step', last_step,'failed')
+            print('halting for 15 minutes, then re-trying from step', self.step)
+            sleep.until_next_quarter_hour()
             self.re_try = False
             self.re_try_counter += 1
-            sleep.until_next_quarter_hour()
             return False
 
-        return True
 
 def mainloop():
     app = Application()
     while 'I`m waiting for coffee':
         app.run()
+
 
 mainloop()
