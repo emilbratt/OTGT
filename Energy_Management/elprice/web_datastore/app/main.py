a89e8bda1bdb2dbe164bb630250927db24722c74
emilbratt <emilbratt@gmail.com>
2023-01-26 17:27:35 +0100

    ADD: simple hello world for web_datastore

--- Energy_Management/elprice/web_datastore/app/main.py
+++ Energy_Management/elprice/web_datastore/app/main.py
@@ -1,8 +1,24 @@
-from time import sleep
+import os
+from io import BytesIO
 
-def main():
-    print('running web_datastore')
-    sleep(10)
+from typing import Optional
+from fastapi import FastAPI, File, Request, status
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse, JSONResponse, StreamingResponse, Response
 
-while True:
-    main()
+POST_TEST_RESPONSE = 'test OK'
+
+class TestModel(BaseModel):
+    # identify caller (android device, browser, hostname etc.)
+    caller: str
+
+app = FastAPI()
+
+@app.get("/test/get")
+def test_get():
+    return {'hello': 'world', 'test': 'OK'}
+
+@app.post("/test/post")
+def test_post(item: TestModel):
+    return {item.caller: POST_TEST_RESPONSE}
