2a92fa7f5b1049e5cc67b7315d4d3a32759c0b0a
emilbratt <emilbratt@gmail.com>
2023-02-23 15:30:00 +0100

    some extra testing

--- Tools/testing/Energy_Management/elprice/main.py
+++ Tools/testing/Energy_Management/elprice/main.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-# a very simple test script
-#
-# some actions are triggered when data is received
-# and this script can also help trigger these actions
-
 import sys
 import requests
 import configparser
-from os import path
 import json
+from os import path
+
+from elspotmetadata  import levels, shapes
+from elspotgenerate  import Generate
+import elspotvisualize
 
 URL_WEB_DATASTORE = 'http://localhost:8085'
 DIR_TEST_DATA = './test_data'
 ELSPOT_RAW = path.join(DIR_TEST_DATA, 'elspot', 'raw_2023-01-23.json')
-ELSPOT_RESHAPED = path.join(DIR_TEST_DATA, 'elspot', 'reshaped_2023-01-23.json')
+ELSPOT_RESHAPED = path.join(DIR_TEST_DATA, 'elspot', 'reshaped_2023-02-21.json')
 PLOT_BYDATE_OSLO = path.join(DIR_TEST_DATA, 'plot', 'bydate_Oslo_2023-01-23.svg')
 PLOT_BYHOUR_OSLO = path.join(DIR_TEST_DATA, 'plot', 'byhour_Oslo_2023-01-23_3.svg')
 STATES_BYDATE_OSLO = path.join(DIR_TEST_DATA, 'states', 'bydate_Oslo_2023-01-23.json')
@@ -55,7 +54,7 @@ def HEAD(URL):
     print_response(r, 'HEAD')
 
 
-def main(test: int):
+def main():
 
     '''
         pass a number when calling this script,
@@ -120,4 +119,75 @@ def main(test: int):
                 postdata['region'] = 'Oslo'
                 postdata['data'] = the_json
                 POST(url, postdata)
-main(test=5)
+        case 7:
+            gnrt = Generate()
+            gnrt.from_val(10)
+            gnrt.to_val(1000)
+            gnrt.fluctuate_level(6)
+            gnrt.spike_level(0)
+            gnrt.dip_level(0)
+            gnrt.set_date('2000-01-01')
+            gnrt.set_region('Oslo')
+            gnrt.split_hours(4)
+            gnrt.sort_prices()
+            gnrt.sort_offset_factor(5)
+            gnrt.initialize()
+            data = gnrt.generate_prices()
+            elspotvisualize.reshaped_overview(data)
+            elspotvisualize.reshaped_horizontal_curve(data)
+
+        case 8:
+            with open(ELSPOT_RESHAPED, 'r') as my_file:
+                data = json.load(my_file)
+                trondheim = data[12]
+                bergen = data[10]
+                oslo = data[8]
+
+                print('Trondheim')
+                elspotvisualize.reshaped_horizontal_curve(trondheim)
+                price_list = [x['value'] for x in trondheim['prices']]
+                slope = shapes.slope(price_list)
+                print('slope', slope)
+                elspotvisualize.percent_bar(slope,0,10)
+                spike = shapes.spike(price_list)
+                print('spike', spike)
+                elspotvisualize.percent_bar(spike,-100,100)
+                print()
+
+                print('Bergen')
+                elspotvisualize.reshaped_horizontal_curve(bergen)
+                price_list = [x['value'] for x in bergen['prices']]
+                slope = shapes.slope(price_list)
+                print('slope', slope)
+                elspotvisualize.percent_bar(slope,0,10)
+                spike = shapes.spike(price_list)
+                print('spike', spike)
+                elspotvisualize.percent_bar(spike,-100,100)
+                print()
+
+        case 9:
+            gnrt = Generate()
+            gnrt.split_hours(4)
+            gnrt.from_val(100)
+            gnrt.to_val(130)
+            gnrt.fluctuate_level(10)
+            gnrt.spike_level(0)
+            gnrt.dip_level(10)
+            gnrt.initialize()
+            data = gnrt.generate_prices()
+            elspotvisualize.reshaped_overview(data)
+            price_list = gnrt.get_price_list()
+            elspotvisualize.horizontal_curve(price_list)
+
+            slope = shapes.slope(price_list)
+            print('slope', slope)
+            elspotvisualize.percent_bar(slope,0,10)
+
+            spike = shapes.spike(price_list)
+            print('spike', spike)
+            elspotvisualize.percent_bar(spike,-100,100)
+            percent_of_max = levels.percent_of_max(price_list)
+            diff_factor_range = levels.diff_factor_range(price_list)
+            weight_range = levels.weight_range(price_list)
+
+main()
