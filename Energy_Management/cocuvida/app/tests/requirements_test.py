75b46bf6f69dd8fd3b6878d3467a966b3b013108
emilbratt <emilbratt@gmail.com>
2023-03-24 20:05:45 +0100

    add aiohttp as requirement (for an async http client) in our app

--- Energy_Management/cocuvida/app/tests/requirements_test.py
+++ Energy_Management/cocuvida/app/tests/requirements_test.py
@@ -3,8 +3,8 @@ import importlib
 
 def check_modules(self):
     self.assertIsNotNone(importlib.util.find_spec('aiofiles'))
+    self.assertIsNotNone(importlib.util.find_spec('aiohttp'))
     self.assertIsNotNone(importlib.util.find_spec('paho'))
-    self.assertIsNotNone(importlib.util.find_spec('requests'))
     self.assertIsNotNone(importlib.util.find_spec('streaming_form_data'))
     self.assertIsNotNone(importlib.util.find_spec('uvicorn'))
     self.assertIsNotNone(importlib.util.find_spec('uvloop'))
