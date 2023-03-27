f538d90eec512ca68a06973f673325d8bfaebe22
emilbratt <emilbratt@gmail.com>
2023-03-27 09:13:19 +0200

    typing

--- Energy_Management/cocuvida/app/cocuvida/sqldatabase/elspot/__init__.py
+++ Energy_Management/cocuvida/app/cocuvida/sqldatabase/elspot/__init__.py
@@ -39,3 +39,46 @@ async def elspot_raw_exists_for_date(isodate: str):
     '''
     res = select_one(query, [isodate])
     return (res[0] == 1)
+
+async def insert_reshaped_elspot(elspot_data: dict):
+    '''
+        pass dict in this format
+        {
+            'region': 'Molde,
+            'currency': 'NOK',
+            'date':    'YYYY-MM-DD',
+            'unit':    'ore/kWh',
+            'Max':     '280',
+            'Min':     '143',
+            'Average': '197',
+            'resolution': 96, # (92 for 23 hours, 100 for 25 hours)
+            'prices': [
+                {'index': 0, 'time_start': '00:00', 'time_end': '00:15', 'value': '210'},
+                {'index': 1, 'time_start': '00:15', 'time_end': '00:30', 'value': '210'},
+                ..,
+                {'index': 95, 'time_start': '23:45', 'time_end': '00:00', 'value': '247'}
+            ]
+        },
+    '''
+    insert_query = '''
+        INSERT INTO elspot_reshaped
+            (elspot_data, elspot_date, elspot_region)
+        VALUES
+            (?, ?, ?)
+    '''
+    update_query = '''
+        UPDATE elspot_reshaped
+        SET elspot_data = ?
+        WHERE elspot_date = ? AND elspot_region = ?
+    '''
+    elspot_region = elspot_data['region']
+    elspot_date = elspot_data['date']
+    elspot_data = json.dumps(elspot_data)
+    res = insert_one(insert_query, [elspot_data, elspot_date, elspot_region])
+    if res == 'insert':
+        return True
+    if res == 'IntegrityError':
+        res = update(update_query, [elspot_data, elspot_date, elspot_region])
+        if res == 'update':
+            return True
+    return False
