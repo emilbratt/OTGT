c98015916d27079b6c01810f2189761c4e2dd0b5
emilbratt <emilbratt@gmail.com>
2023-05-13 18:10:29 +0200

    ignore auth when envar COCUVIDA_TESTING is set (for now at least)

--- Energy_Management/cocuvida/app/cocuvida/authorize/__init__.py
+++ Energy_Management/cocuvida/app/cocuvida/authorize/__init__.py
@@ -1,6 +1,8 @@
-from cocuvida.environment import env_ini_get
+from cocuvida.environment import env_ini_get, env_var_get
 
 
 def check_secret(secret: str) -> bool:
+    if env_var_get('COCUVIDA_TESTING') == True:
+        return True
     stored_secret = env_ini_get('cocuvida', 'secret')
     return (secret == stored_secret)
