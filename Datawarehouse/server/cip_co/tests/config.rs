6b8cd612d8898713587691d967f6d4b2a1062606
emilbratt <emilbratt@gmail.com>
2026-03-04 20:17:04 +0100

    cip_co: config test stuff

--- Datawarehouse/server/cip_co/tests/config.rs
+++ Datawarehouse/server/cip_co/tests/config.rs
@@ -4,5 +4,10 @@ use cip_co::CONFIG;
 
 #[test]
 fn config() {
-    &CONFIG;
+    // verify values exist..
+    assert!(!CONFIG.port.is_empty());
+    assert!(!CONFIG.address.is_empty());
+    assert!(!CONFIG.retail_db_user.is_empty());
+    assert!(!CONFIG.retail_db_pwd.is_empty());
+    assert!(CONFIG.use_dummy_data || !CONFIG.use_dummy_data);
 }
